8680b348c5aba9b509cc74fe5b2acedf6aff20d2
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-03 01:37:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
-list = ["ab", "cd", "ef"]
-
 def my_each(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i = i + 1
+    i += 1
   end
+  array
 end
-
-my_each(list) {|i| puts "So I think #{i} is a piece of shit too"}
