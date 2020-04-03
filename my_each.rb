9e837958a75059a703688962470771b7b1757657
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-03 01:33:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,39 +1,11 @@
+list = ["ab", "cd", "ef"]
+
 def my_each(array)
-  count = 0
-  collected = []
-  while count < i.length
-    yield[count]
-    count += 1
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
-  collected
 end
 
-my_each(array){|arg| arg}
-
- collection = [1, 2, 3, 4]
-
- def my_each(array)
-   count = 0
-   collected = []
-   while count < array.length
-     yield
-     count += 1
-   end
-   collected
- end
-
- my_each(collection){|col| collected << col[count]}
-
-
-random = [ 2, 1, 4, 6, 3, 9]
- def each_method(array)
-   count = 0
-   collected = []
-   while count < array.length
-     collected << array[count]
-     count += 1
-   end
-   yield(collected)
- end
-
- each_method(collected){|col| col.sort}
+my_each(list) {|i| puts "So I think #{i} is a piece of shit too"}
