3c8e29b1c9c635d9440da88d71be8659e7daf953
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-03 01:31:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,39 @@
-def my_each(array) do |i|
+def my_each(array) 
   count = 0
-  while count < array.lenghth
+  collected = []
+  while count < i.length
+    yield[count]
+    count += 1
   end
+  collected
 end
+
+my_each(array){|arg| arg}
+
+ collection = [1, 2, 3, 4]
+
+ def my_each(array)
+   count = 0
+   collected = []
+   while count < array.length
+     yield
+     count += 1
+   end
+   collected
+ end
+
+ my_each(collection){|col| collected << col[count]}
+
+
+random = [ 2, 1, 4, 6, 3, 9]
+ def each_method(array)
+   count = 0
+   collected = []
+   while count < array.length
+     collected << array[count]
+     count += 1
+   end
+   yield(collected)
+ end
+ 
+ each_method(collected){|col| col.sort}
